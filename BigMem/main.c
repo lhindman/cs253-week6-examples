c0eaaefc7f7cad941f416c3a80dc6350a4f6fa19
Luke Hindman <lukehindman@boisestate.edu>
2020-09-24 14:09:45 -0700

    Initial commit of week 6 examples

---
+++ BigMem/main.c
@@ -0,0 +1,16 @@
+/* 
+ * Author:  Luke Hindman
+ * Date: Thu 24 Sep 2020 12:35:45 PM PDT
+ * Description: Demostrate allocating a large global array
+ */
+#include <stdio.h>
+
+
+char bigGlobalData[1024 * 1024 * 1024];
+
+int main(void) {
+
+   printf("The size of bigData is: %ld\n", sizeof(bigGlobalData));
+
+   return 0;
+}
